48e06d94213d6c2005b33437c42242ef74965dd0
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-04-22 23:22:17 -0300

    add: tipagem no knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line
+import { Knex } from 'knex'
+// ou faça apenas:
+// import 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
+  }
+}
